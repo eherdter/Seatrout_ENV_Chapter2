62a099ad88d229a0f0fcce26a1ba838fb4637f8f
Herdter <Elizabeth.Herdter@myfwc.com>
2017-11-14 15:46:31 -0500

    Pushing again to git

---
+++ All_ENV_vars_FIM.R
@@ -0,0 +1,149 @@
+# R script for getting all of the FIM habitat and environmental variables together.
+# Some of this script borrows from Delta_Method_For_Producing_Nominal
+
+##### SET WORKING DIRECTORY_YOY ####
+#must change working directory for data when working on personal vs work computer
+setwd("~/Desktop/PhD project/Projects/Seatrout/FWRI SCRATCH FOLDER/Elizabeth Herdter/SAS data sets/FIMData/NEWNov7")
+setwd("U:/PhD_projectfiles/Raw_Data/Seatrout_FIM_Data/FIMData/NEWNov7")
+
+##### SET PACKAGES ######################
+library(haven) #to load sas
+library(dplyr) # to do df manipulation
+
+##### IMPORT DATA SETS_YOY ######
+# These data sets were produced using the spp_comb_5_13_EG_2bays_yoy_2015_EHedits.sas program which is stored in my scratch folder
+# Bay and river stations were denoted by the gear sampling  code. There is a hard copy of this script in my green folder. 
+#11/7/16- Realized that the data sets that were produced by the above sas program that I edited was pretty crummy in that
+#         a lot of YOY animals were being excluded by my gear selections. Therefore I went back to FWRI and was more general with my gear selections.
+#         Chose gears 19,20,22,23 following what gets selected by the spp_comb_5_13_EG_2bays_yoy_2015.sas program 
+
+# select the important recruitment months for each zone and also check on gear codes
+# adjust selected gear if it was not chosen in the first place
+# _C$month => depends on recruitment window in each estuary
+#               => Jax 5<=x<=11
+#               => nor. IRL 5<=x<=11
+#               => CK  5<=x<=11
+#               => TB  4<=x<=10
+#               => CH  4<=x<=10
+#               => AP  6<=x<=10
+
+#load the data, select the peak reproductive months, reorder columns alphabetically so I can combine dataframes (some columns were in different position in other df)
+# left join the datasets with the hydro observations by the variable  "Reference" 
+
+ap = subset(read_sas("ap_yoy_cn_c.sas7bdat"), month %in% c(6,7,8,9,10,11)) %>% mutate(bUnk=bunk) %>% select(-bunk) 
+ap <- ap %>% select(noquote(order(colnames(ap))))  #reorders the columns alphabetically 
+ap_hyd <- subset(read_sas("ap_yoy_cn_hyd.sas7bdat")) 
+ap <- left_join(ap, ap_hyd)
+
+ck = subset(read_sas("ck_yoy_cn_c.sas7bdat"),  month %in% c(5,6,7,8,9,10,11))
+ck <- ck %>% select(noquote(order(colnames(ck))))
+ck_hyd <- subset(read_sas("ck_yoy_cn_hyd.sas7bdat")) 
+ck <- left_join(ck, ck_hyd)
+
+tb = subset(read_sas("tb_yoy_cn_c.sas7bdat"), month %in% c(4,5,6,7,8,9,10)) 
+tb <- tb %>% select(noquote(order(colnames(tb))))
+tb_hyd <- subset(read_sas("tb_yoy_cn_hyd.sas7bdat")) 
+tb <- left_join(tb, tb_hyd)
+
+ch = subset(read_sas("ch_yoy_cn_c.sas7bdat"), month %in% c(4,5,6,7,8,9,10)) %>% mutate(bUnk=bunk) %>% select(-bunk) 
+ch <- ch %>% select(noquote(order(colnames(ch))))
+ch_hyd <- subset(read_sas("ch_yoy_cn_hyd.sas7bdat")) 
+ch <- left_join(ch, ch_hyd)
+
+ir = subset(read_sas("ir_yoy_cn_c.sas7bdat"), month %in% c(5,6,7,8,9,10,11)) 
+ir <- ir %>% select(noquote(order(colnames(ir))))
+ir_hyd <- subset(read_sas("ir_yoy_cn_hyd.sas7bdat")) 
+ir <- left_join(ir, ir_hyd)
+
+jx = subset(read_sas("jx_yoy_cn_c.sas7bdat") , month %in% c(5,6,7,8,9,10,11)) 
+jx <- jx %>% select(noquote(order(colnames(jx))))
+jx_hyd <- subset(read_sas("jx_yoy_cn_hyd.sas7bdat")) 
+jx <- left_join(jx, jx_hyd)
+
+full <- rbind(ap, ck, tb, ch, ir, jx)
+
+##### SELECT CATEGORICAL HABITAT VALUES ########
+
+#This section is nearly identical to that in Delta Method script
+#Based on FWRI code the three variables were bottom type (bStr, bsan, bmud), bottom vegetation (bveg), and shoreline (Shore)
+#There are three different bottom type variables each of them coded in a binary form.
+#I want to take bStr, bsan, and bmud and put them into 1 variable so I will make a new variable entirely = 'bottom'
+#I also want to turn bveg into a new variable = 'veg' based on the entries. If alg or Sav then turn to SAV because there are only 9 entries for Alg. 
+#Same thing for the shore variable = 'shore'. Decided to have only emergent, structure, terrestrial, and mangrove. 
+#Removed old variables (bStr, bSan, bMud, bveg, Shore)
+#Removed rows when there was no shoreline variable. 
+
+full <-  select(full, c(number,year, month, bio_reference, bStr, bSan, bMud, bveg, Shore, bay, Zone)) %>% 
+  mutate(bottom = ifelse(full$bStr ==1, "structure", ifelse(full$bSan>0 | full$bMud>0, "mudsand", "unknown")), 
+         veg= ifelse(full$bveg == "SAVAlg", "SAV", ifelse(full$bveg == "Alg", "SAV", ifelse(full$bveg =="SAV", "SAV", "Noveg"))),
+         shore = ifelse(substr(full$Shore,1,3)=="Eme", "Emerge", ifelse(substr(full$Shore,1,3) =="Man", "Mangrove", ifelse(substr(full$Shore,1,3)=="Str", "Structure", 
+                                                                                                                           ifelse(substr(full$Shore, 1,3)=="Ter", "Terrestrial", "Non"))))) %>% select(-c(bStr, bSan, bMud, bveg, Shore)) %>% subset(!shore=="Non") 
+
+#Turn habitat variables into factors so they can be treated as categorical
+full[,c(2,5:9)] <- lapply(full[,c(2,5:9)], factor)
+
+#select from full the estuaries so that the categorical varaibles can be sorted to make sure that there are enough observations per level of each
+ap.fl <- droplevels(full %>% subset(bay =='AP'))
+ck.fl <- droplevels(full %>% subset(bay =='CK'))
+tb.fl <- droplevels(full %>% subset(bay =='TB'))
+ch.fl <- droplevels(full %>% subset(bay =='CH'))
+jx.fl <- droplevels(full %>% subset(bay =='JX'))
+ir.fl <- droplevels(full %>% subset(bay =='IR'))
+
+with(ap.fl,tapply(number, list(year,month),sum))
+with(ap.fl,tapply(number, list(year,veg),sum))
+with(ap.fl,tapply(number, list(year,bottom),sum))
+with(ap.fl,tapply(number, list(year,shore),sum))
+ap.fl <- subset(ap.fl, shore != "Mangrove") %>% droplevels(ap.fl$shore)
+ap.fl$month <- as.factor(as.character(ap.fl$month))
+
+
+with(ck.fl,tapply(number, list(year,month),sum))
+with(ck.fl,tapply(number, list(year,veg),sum))
+with(ck.fl,tapply(number, list(year,bottom),sum))
+ck.fl = droplevels(subset(ck.fl, bottom != "unknown"))
+with(ck.fl,tapply(number, list(year,shore),sum))
+#drop terrestrial and join structure 
+ck.fl <- droplevels(subset(ck.fl, shore != "Terrestrial"))
+ck.fl$shore[ck.fl$shore== "Mangrove"] = "Structure"
+ck.fl <- droplevels(subset(ck.fl, shore != "Mangrove"))
+ck.fl$month <- as.factor(as.character(ck.fl$month))
+
+with(tb.fl,tapply(number, list(year,month),sum))
+with(tb.fl,tapply(number, list(year,veg),sum))
+with(tb.fl,tapply(number, list(year,bottom),sum))
+tb.fl <- droplevels(subset(tb.fl, bottom != "unknown"))
+with(tb.fl,tapply(number, list(year,shore),sum))
+tb.fl$month <- as.factor(as.character(tb.fl$month))
+
+with(ch.fl,tapply(number, list(year,month),sum))
+with(ch.fl,tapply(number, list(year,veg),sum))
+with(ch.fl,tapply(number, list(year,bottom),sum))
+with(ch.fl,tapply(number, list(year,shore),sum))
+ch.fl$month <- as.factor(as.character(ch.fl$month))
+
+with(jx.fl,tapply(number, list(year,month),sum))
+with(jx.fl,tapply(number, list(year,veg),sum))
+with(jx.fl,tapply(number, list(year,bottom),sum))
+jx.fl <- droplevels(subset(jx.fl, bottom != "unknown"))
+with(jx.fl,tapply(number, list(year,shore),sum))
+jx.fl <- na.omit(jx.fl)
+jx.fl$month <- as.factor(as.character(jx.fl$month))
+
+with(ir.fl,tapply(number, list(year,month),sum))
+with(ir.fl,tapply(number, list(year,veg),sum))
+with(ir.fl,tapply(number, list(year,bottom),sum))
+with(ir.fl,tapply(number, list(year,shore),sum))
+ir.fl <- na.omit(ir.fl)
+ir.fl$month <- as.factor(as.character(ir.fl$month))
+
+#Notes ###
+# depending on how variables will be modeled in a gam or something else will dictate how I should pre-process the environmental variables
+
+# glm was modeled as total number of seatrout per haul,
+# then, lsmeans produced yearly estimates of # seatrout per haul
+
+
+
+
+
