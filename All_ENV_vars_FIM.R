25efa916400baeca0db73cf4393904c5a77fa383
Herdter <Elizabeth.Herdter@myfwc.com>
2017-11-15 15:39:44 -0500

    calculated extinction coefficient for turbidity proxy

--- All_ENV_vars_FIM.R
+++ All_ENV_vars_FIM.R
@@ -1,76 +1,100 @@
+# ABOUT ####
 # R script for getting all of the FIM habitat and environmental variables together.
 # Some of this script borrows from Delta_Method_For_Producing_Nominal
+# Environmental variables collected by FIM include: Dissolved O2, Salinity, Temp, pH, secchi depth
 
-##### SET WORKING DIRECTORY_YOY ####
+# SET WORKING DIRECTORY ####
 #must change working directory for data when working on personal vs work computer
 setwd("~/Desktop/PhD project/Projects/Seatrout/FWRI SCRATCH FOLDER/Elizabeth Herdter/SAS data sets/FIMData/NEWNov7")
 setwd("U:/PhD_projectfiles/Raw_Data/Seatrout_FIM_Data/FIMData/NEWNov7")
 
-##### SET PACKAGES ######################
+# SET PACKAGES ####
 library(haven) #to load sas
 library(dplyr) # to do df manipulation
 
-##### IMPORT DATA SETS_YOY ######
+# IMPORT DATA SETS ####
 # These data sets were produced using the spp_comb_5_13_EG_2bays_yoy_2015_EHedits.sas program which is stored in my scratch folder
-# Bay and river stations were denoted by the gear sampling  code. There is a hard copy of this script in my green folder. 
-#11/7/16- Realized that the data sets that were produced by the above sas program that I edited was pretty crummy in that
-#         a lot of YOY animals were being excluded by my gear selections. Therefore I went back to FWRI and was more general with my gear selections.
-#         Chose gears 19,20,22,23 following what gets selected by the spp_comb_5_13_EG_2bays_yoy_2015.sas program 
+# For more description see Delta_Method_for_Producing R script 
 
 # select the important recruitment months for each zone and also check on gear codes
-# adjust selected gear if it was not chosen in the first place
 # _C$month => depends on recruitment window in each estuary
 #               => Jax 5<=x<=11, => nor. IRL 5<=x<=11, => CK  5<=x<=11, => TB  4<=x<=10, => CH  4<=x<=10, => AP  6<=x<=10
 
-# load the data, select the peak reproductive months, reorder columns alphabetically so I can combine dataframes (some columns were in different position in other df)
-# load the hydro dataset that contains Depth where the observation was taken, Temperature, Conductivity, pH, Salinity, Dissolved O2
-# load the 
-# left join the physical data set that contains secchi depth, was secchi on bottom, depth (Depth) where hydrolab observation was taken
-# Note: this Depth is different then depth of location (That is StartDepth and EndDepth)
+# Load the data, select the peak reproductive months.
+# Load the hydro dataset that contains Depth where the observation was taken, Temperature, Conductivity, pH, Salinity, Dissolved O2
+# Load the physicical dataset that contains secchi depth, was secchi on bottom, depth (Depth) where hydrolab observation was taken
+        # Note: this Depth is different then depth of location (That is StartDepth and EndDepth)
+        # The physical data set is newly added so it contains additional sampling years but this should be taken care of when I join be reference. 
+# There are some duplicated reference numbers in ap_hyd because the hydrolab took a few obsercations at each haul so I'll
+# just have to chose one- drop all others for that reference. 
+# Left join them by reference.
+# Careful to see that the size of the original catch dataset doesn't change in size because we want to keep all of the catch 
+# observations and not drop any that maybe do not have associated enviro variables or add observations of enviro variables to referecence numbers
+# Reorder columns alphabetically so I can combine dataframes (some columns were in different position in other df)
 
 ap = subset(read_sas("ap_yoy_cn_c.sas7bdat"), month %in% c(6,7,8,9,10,11)) %>% mutate(bUnk=bunk) %>% select(-bunk) 
-ap <- ap %>% select(noquote(order(colnames(ap))))  #reorders the columns alphabetically 
 ap_hyd <- subset(read_sas("ap_yoy_cn_hyd.sas7bdat")) 
-ap_phys <- read_sas("~/Desktop/PhD project/Projects/Seatrout/Data/Raw Survey Data/Seatrout FIM Data/apm_physical.sas7bdat") %>% select(Reference, Secchi_on_bottom, Secchi_depth)
+#ap_phys <- read_sas("~/Desktop/PhD project/Projects/Seatrout/Data/Raw Survey Data/Seatrout FIM Data/apm_physical.sas7bdat") %>% select(Reference, Secchi_on_bottom, Secchi_depth)
+ap_phys <- read_sas("U:/PhD_projectfiles/Raw_Data/Seatrout_FIM_Data/Raw Data from fimaster-data-sas-inshore/apm_physical.sas7bdat") %>% select(Reference, Secchi_on_bottom, Secchi_depth)
 ap_phys$Reference <- as.character(ap_phys$Reference)
 #There are duplicated References in ap_hyd
 ap_hyd <- ap_hyd[!duplicated(ap_hyd$Reference),]
-test <- left_join(ap, ap_hyd)
-
 ap <- left_join(ap, ap_phys, by="Reference") %>% left_join(ap_hyd, by="Reference")
-
-
-
-
+ap <- ap %>% select(noquote(order(colnames(ap))))  #reorders the columns alphabetically 
 
 ck = subset(read_sas("ck_yoy_cn_c.sas7bdat"),  month %in% c(5,6,7,8,9,10,11))
-ck <- ck %>% select(noquote(order(colnames(ck))))
+#ck_phys <- read_sas("~/Desktop/PhD project/Projects/Seatrout/Data/Raw Survey Data/Seatrout FIM Data/ckm_physical.sas7bdat") %>% select(Reference, Secchi_on_bottom, Secchi_depth)
+ck_phys <- read_sas("U:/PhD_projectfiles/Raw_Data/Seatrout_FIM_Data/Raw Data from fimaster-data-sas-inshore/ckm_physical.sas7bdat") %>% select(Reference, Secchi_on_bottom, Secchi_depth)
+ck_phys$Reference <- as.character(ck_phys$Reference)
 ck_hyd <- subset(read_sas("ck_yoy_cn_hyd.sas7bdat")) 
-ck <- left_join(ck, ck_hyd)
+#Also duplicated References in ck_hyd
+ck_hyd <- ck_hyd[!duplicated(ck_hyd$Reference),]
+ck <- left_join(ck, ck_phys, by="Reference") %>% left_join(ck_hyd, by="Reference")
+ck <- ck %>% select(noquote(order(colnames(ck))))  #reorders the columns alphabetically 
 
 tb = subset(read_sas("tb_yoy_cn_c.sas7bdat"), month %in% c(4,5,6,7,8,9,10)) 
-tb <- tb %>% select(noquote(order(colnames(tb))))
+#tb_phys <- read_sas("~/Desktop/PhD project/Projects/Seatrout/Data/Raw Survey Data/Seatrout FIM Data/tbm_physical.sas7bdat") %>% select(Reference, Secchi_on_bottom, Secchi_depth)
+tb_phys <- read_sas("U:/PhD_projectfiles/Raw_Data/Seatrout_FIM_Data/Raw Data from fimaster-data-sas-inshore/tbm_physical.sas7bdat") %>% select(Reference, Secchi_on_bottom, Secchi_depth)
+tb_phys$Reference <- as.character(tb_phys$Reference)
 tb_hyd <- subset(read_sas("tb_yoy_cn_hyd.sas7bdat")) 
-tb <- left_join(tb, tb_hyd)
+#Also duplicated References in tb_hyd
+tb_hyd <- tb_hyd[!duplicated(tb_hyd$Reference),]
+tb <- left_join(tb, tb_phys, by="Reference") %>% left_join(tb_hyd, by="Reference")
+tb <- tb %>% select(noquote(order(colnames(tb))))  #reorders the columns alphabetically 
 
 ch = subset(read_sas("ch_yoy_cn_c.sas7bdat"), month %in% c(4,5,6,7,8,9,10)) %>% mutate(bUnk=bunk) %>% select(-bunk) 
-ch <- ch %>% select(noquote(order(colnames(ch))))
+#ch_phys <- read_sas("~/Desktop/PhD project/Projects/Seatrout/Data/Raw Survey Data/Seatrout FIM Data/chm_physical.sas7bdat") %>% select(Reference, Secchi_on_bottom, Secchi_depth)
+ch_phys <- read_sas("U:/PhD_projectfiles/Raw_Data/Seatrout_FIM_Data/Raw Data from fimaster-data-sas-inshore/chm_physical.sas7bdat") %>% select(Reference, Secchi_on_bottom, Secchi_depth)
+ch_phys$Reference <- as.character(ch_phys$Reference)
 ch_hyd <- subset(read_sas("ch_yoy_cn_hyd.sas7bdat")) 
-ch <- left_join(ch, ch_hyd)
+#Also duplicated References in ch_hyd
+ch_hyd <- ch_hyd[!duplicated(ch_hyd$Reference),]
+ch <- left_join(ch, ch_phys, by="Reference") %>% left_join(ch_hyd, by="Reference")
+ch <- ch %>% select(noquote(order(colnames(ch))))  #reorders the columns alphabetically 
 
 ir = subset(read_sas("ir_yoy_cn_c.sas7bdat"), month %in% c(5,6,7,8,9,10,11)) 
-ir <- ir %>% select(noquote(order(colnames(ir))))
+#ir_phys <- read_sas("~/Desktop/PhD project/Projects/Seatrout/Data/Raw Survey Data/Seatrout FIM Data/irm_physical.sas7bdat") %>% select(Reference, Secchi_on_bottom, Secchi_depth)
+ir_phys <- read_sas("U:/PhD_projectfiles/Raw_Data/Seatrout_FIM_Data/Raw Data from fimaster-data-sas-inshore/irm_physical.sas7bdat") %>% select(Reference, Secchi_on_bottom, Secchi_depth)
+ir_phys$Reference <- as.character(ir_phys$Reference)
 ir_hyd <- subset(read_sas("ir_yoy_cn_hyd.sas7bdat")) 
-ir <- left_join(ir, ir_hyd)
+#Also duplicated References in ir_hyd
+ir_hyd <- ir_hyd[!duplicated(ir_hyd$Reference),]
+ir <- left_join(ir, ir_phys, by="Reference") %>% left_join(ir_hyd, by="Reference")
+ir <- ir %>% select(noquote(order(colnames(ir))))  #reorders the columns alphabetically 
 
 jx = subset(read_sas("jx_yoy_cn_c.sas7bdat") , month %in% c(5,6,7,8,9,10,11)) 
-jx <- jx %>% select(noquote(order(colnames(jx))))
+#jx_phys <- read_sas("~/Desktop/PhD project/Projects/Seatrout/Data/Raw Survey Data/Seatrout FIM Data/jxm_physical.sas7bdat") %>% select(Reference, Secchi_on_bottom, Secchi_depth)
+jx_phys <- read_sas("U:/PhD_projectfiles/Raw_Data/Seatrout_FIM_Data/Raw Data from fimaster-data-sas-inshore/jxm_physical.sas7bdat") %>% select(Reference, Secchi_on_bottom, Secchi_depth)
+jx_phys$Reference <- as.character(jx_phys$Reference)
 jx_hyd <- subset(read_sas("jx_yoy_cn_hyd.sas7bdat")) 
-jx <- left_join(jx, jx_hyd)
+#Also duplicated References in jx_hyd
+jx_hyd <- jx_hyd[!duplicated(jx_hyd$Reference),]
+jx <- left_join(jx, jx_phys, by="Reference") %>% left_join(jx_hyd, by="Reference")
+jx <- jx %>% select(noquote(order(colnames(jx))))  #reorders the columns alphabetically 
 
 full <- rbind(ap, ck, tb, ch, ir, jx)
 
-##### SELECT CATEGORICAL HABITAT VALUES ########
+# AGGREGATE CATEGORICAL HABITAT VARIABLES ########
 
 #This section is nearly identical to that in Delta Method script
 #Based on FWRI code the three variables were bottom type (bStr, bsan, bmud), bottom vegetation (bveg), and shoreline (Shore)
@@ -81,11 +105,13 @@ full <- rbind(ap, ck, tb, ch, ir, jx)
 #Removed old variables (bStr, bSan, bMud, bveg, Shore)
 #Removed rows when there was no shoreline variable. 
 
-full <-  select(full, c(number,year, month, bio_reference, bStr, bSan, bMud, bveg, Shore, bay, Zone)) %>% 
+full <-  full %>% 
   mutate(bottom = ifelse(full$bStr ==1, "structure", ifelse(full$bSan>0 | full$bMud>0, "mudsand", "unknown")), 
          veg= ifelse(full$bveg == "SAVAlg", "SAV", ifelse(full$bveg == "Alg", "SAV", ifelse(full$bveg =="SAV", "SAV", "Noveg"))),
-         shore = ifelse(substr(full$Shore,1,3)=="Eme", "Emerge", ifelse(substr(full$Shore,1,3) =="Man", "Mangrove", ifelse(substr(full$Shore,1,3)=="Str", "Structure", 
-                                                                                                                           ifelse(substr(full$Shore, 1,3)=="Ter", "Terrestrial", "Non"))))) %>% select(-c(bStr, bSan, bMud, bveg, Shore)) %>% subset(!shore=="Non") 
+         shore = ifelse(substr(full$Shore,1,3)=="Eme", "Emerge", ifelse(substr(full$Shore,1,3) =="Man", "Mangrove", 
+          ifelse(substr(full$Shore,1,3)=="Str", "Structure", ifelse(substr(full$Shore, 1,3)=="Ter", "Terrestrial", "Non")))))    %>%
+        select(-c(bStr, bSan, bMud, bveg, Shore)) %>% subset(!shore=="Non") %>% 
+          mutate(avgDepth = mean(c(StartDepth, Enddepth)))
 
 #Turn habitat variables into factors so they can be treated as categorical
 full[,c(2,5:9)] <- lapply(full[,c(2,5:9)], factor)
@@ -104,7 +130,6 @@ with(ap.fl,tapply(number, list(year,bottom),sum))
 with(ap.fl,tapply(number, list(year,shore),sum))
 ap.fl <- subset(ap.fl, shore != "Mangrove") %>% droplevels(ap.fl$shore)
 ap.fl$month <- as.factor(as.character(ap.fl$month))
-
 
 with(ck.fl,tapply(number, list(year,month),sum))
 with(ck.fl,tapply(number, list(year,veg),sum))
@@ -145,11 +170,11 @@ with(ir.fl,tapply(number, list(year,shore),sum))
 ir.fl <- na.omit(ir.fl)
 ir.fl$month <- as.factor(as.character(ir.fl$month))
 
-#Notes ###
-# depending on how variables will be modeled in a gam or something else will dictate how I should pre-process the environmental variables
+# PROCUDE ATTENUATION COEFFICIENT FROM SECCHI DEPTH ####
+# Only want to do this for Secchi_on_bottom = NO
+full <- full %>% mutate(ext_ceof = ifelse(Secchi_on_bottom =="NO", 1.7/(Secchi_depth), NA))
 
-# glm was modeled as total number of seatrout per haul,
-# then, lsmeans produced yearly estimates of # seatrout per haul
+
 
 
 
