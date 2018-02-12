f52269c21e1e1a4e5b1b437969636b6f08669495
Herdter <Elizabeth.Herdter@myfwc.com>
2018-02-12 09:28:09 -0500

    adding new script that does exploration plots and BRT on the dadataset produced by Join_Env_Vars_with_FIM.R

---
+++ exploration_plots_and_BRT.R
@@ -0,0 +1,25 @@
+# code to do boosted regression trees on the data produced in Join_Env_Vars_with_FIM.R
+
+# TB ####
+
+#load data
+
+#select only variables that are important (response and predictor variables )
+
+#Explore predictor data in plots 
+
+#Explore response data in plots 
+
+#Make some plots 
+
+
+# do boosted regression
+
+
+
+
+
+# CH ####
+
+
+# 
