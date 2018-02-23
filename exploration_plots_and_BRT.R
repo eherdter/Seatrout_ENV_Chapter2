21515cab1d3190bd6f4c188b2f561b8b37079120
Herdter <Elizabeth.Herdter@myfwc.com>
2018-02-23 16:48:10 -0500

    BRT script with additional notes

--- exploration_plots_and_BRT.R
+++ exploration_plots_and_BRT.R
@@ -6,6 +6,67 @@
 
 #select only variables that are important (response and predictor variables )
 
+# number
+# 
+# salinity  (FIM-at moment)
+# temperature (FIM - at moment)
+# river_flow (mean monthly flow rate)
+# Nit_val (month)
+# Phos_val (month)
+# Sal_val  (month)
+# WatTemp  (month)
+# Z_val    (month)
+# Z_anom   (month)
+# MaxT_val  (month)
+# MaxT_anom  (month)
+# MinT_val   (month)
+# MinT_anom   (month)
+# ToTMonthlyRF  (month)
+# ext_coef
+# spring_dis
+# summer_dis
+# winter_dis
+# prev_autumn_dis
+# spring_Z_val
+# spring_Z_anom
+# spring_MaxT_val	
+# spring_MaxT_anom	
+# spring_MinT_val	
+# spring_MinT_anom	
+# summer_Z_val	
+# summer_Z_anom	
+# summer_MaxT_val	
+# summer_MaxT_anom	
+# summer_MinT_val	
+# summer_MinT_anom	
+# winter_Z_val	
+# winter_Z_anom	
+# winter_MaxT_val	
+# winter_MaxT_anom	
+# winter_MinT_val	
+# winter_MinT_anom	
+# prev_autumn_Z_val	
+# prev_autumn_Z_anom	
+# prev_autumn_MaxT_val	
+# prev_autumn_MaxT_anom	
+# prev_autumn_MinT_val	
+# prev_autumn_MinT_anom	
+# spring_RF	
+# summer_RF	
+# winter_RF	
+# prev_autumn_RF	
+# spring_dis_ALL	
+# summer_dis_ALL	
+# winter_dis_ALL	
+# prev_autumn_dis_ALL	
+# first_spawn_salinity	
+# first_spawn_waterT	
+# second_spawn_salinity	
+# second_spawn_waterT	
+# third_spawn_salinity	
+# third_spawn_waterT
+
+
 #Explore predictor data in plots 
 
 #Explore response data in plots 
